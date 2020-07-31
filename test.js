556a87d7af84bc724d4eb4c1dc1b517b749a629c
Leonard Ehrenfried <mail@leonard.io>
2020-07-31 16:42:28 +0200

    Add test

---
+++ test.js
@@ -0,0 +1,17 @@
+var assert = require("assert");
+var OtpCityBikeSource = require("./index");
+
+describe("CityBikeSource", function() {
+  it("fetch data", (done) => {
+    const url = new URL("https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql");
+    const source = new OtpCityBikeSource(url, () => {});
+    assert.ok(source);
+
+    // request tile in Helsinki
+    source.getTile(14, 9326, 4741, function(err, response){
+      assert.ok(response.length > 100);
+      assert.ok(response);
+      done();
+    })
+  });
+});
